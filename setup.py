b60d76eb83d614ef4ded61fa949f33cdfcc269f5
Ed Herbst <ed.herbst@gmail.com>
2015-06-05 15:22:20 -0400

    update setup

--- setup.py
+++ setup.py
@@ -9,20 +9,20 @@ from numpy.distutils.system_info import get_info
 try:
     blas_opt = get_info('blas_mkl',notfound_action=2)
     lapack_opt = get_info('lapack_mkl',notfound_action=2)
+
+    extra_info = get_info('mkl', 2)
+
 except:
     blas_opt = get_info('blas',notfound_action=2)
     lapack_opt = get_info('lapack',notfound_action=2)
 
     
-libs = [blas_opt['libraries'][0], lapack_opt['libraries'][0]]
+libs = [blas_opt['libraries'][0], lapack_opt['libraries'][0]] + extra_info['libraries']
 lib_dirs = [blas_opt['library_dirs'][0], lapack_opt['library_dirs'][0]]
 
-
-#buildpath =os.path.dirname(os.path.path(__file__))
-#print buildpath
 ext1 = Extension(name = 'dsge.fortran.cholmod',
                  sources = ['dsge/fortran/cholmod.f90'],
-                 libraries = ['gensys'] + libs)
+                 libraries = libs)
 ext2 = Extension(name = 'dsge.fortran.gensysw',
                  sources = ['dsge/fortran/gensys_wrapper.f90'],
                  libraries = ['gensys'] + libs, 
@@ -31,7 +31,7 @@ ext2 = Extension(name = 'dsge.fortran.gensysw',
 ext3 = Extension(name = 'dsge.fortran.filter',
                  sources = ['dsge/fortran/kf_fortran.f90'],
                  libraries = ['slicot'] + libs,
-                 library_dirs = ['/mq/home/m1eph00/lib']
+                 library_dirs = ['/mq/home/m1eph00/lib'],
                  )
 ext4 = Extension(name = 'dsge.fortran.dlyap',
                  sources = ['dsge/fortran/dlyap_wrapper.f90'],
@@ -45,14 +45,13 @@ if __name__ == "__main__":
     numpy.distutils.core.setup(
 
         name = 'dsge',
-        version = '0.0.1',
+        version = '0.0.2',
         platforms = 'linux',
         libraries = [('gensys',
                       {'sources': ['dsge/fortran/gensys.f90'],
                    })],
-        packages = ['dsge'],
+        packages = ['dsge', 'dsge.tests'],
         ext_modules = [ext1,ext2,ext3,ext4],
-
         test_suite='nose.collector',
         tests_require=['nose']
     )
