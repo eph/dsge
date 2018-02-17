2b0c6dbe84613223a393bf78282ee2b0ef3924ca
Ed Herbst <ed.herbst@gmail.com>
2018-02-16 20:14:34 -0500

    clean up tests

--- dsge/tests/test_gensys.py
+++ dsge/tests/test_gensys.py
@@ -1,26 +1,16 @@
-from __future__ import division
+<from __future__ import division
 
 import numpy as np
 from numpy.testing import assert_equal, assert_array_almost_equal
 
 from unittest import TestCase
 
-#from dsge.fortran import gensysw
 from dsge.DSGE import DSGE
-#from dsge.gensys import gensy
+
 import pkg_resources
 
 class TestGensys(TestCase):
 
-    def test_simple(self):
-        RR = np.eye(3)
-        TT = np.zeros((3,3))
-
-
-        #TT, CC, RR, fmat, fwt, ywt, gev, RC, loose = gensysw.gensys_wrapper.call_gensys(G0, G1, C0, PSI, PPI, 1.00000000001)
-
-        #self.assertEqual(RC,0)
-        #assert_equal(np.eye(3),PP)
 
     def test_pc(self):
         relative_loc = ('examples/schorf_phillips_curve/'
