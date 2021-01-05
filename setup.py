995807564e59247782c85f8433c4e7c4acd8285a
Ed Herbst <ed.herbst@gmail.com>
2021-01-05 18:49:24 -0500

    setuptools before distutils

--- setup.py
+++ setup.py
@@ -1,37 +1,34 @@
+import setuptools
 import numpy.distutils.core
+# def build_ext(config):
 
-from setuptools import find_packages
-
-
-def build_ext(config):
-
-    config_dict = config.todict()
-    config_dict.pop("name")
-    return config_dict
+#     config_dict = config.todict()
+#     config_dict.pop("name")
+#     return config_dict
 
 
 if __name__ == "__main__":
 
     from numpy.distutils.misc_util import Configuration
 
-    config_dict = build_ext(Configuration("dsge", parent_package=None, top_path=None))
+    #config_dict = build_ext(Configuration("dsge", parent_package=None, top_path=None))
     numpy.distutils.core.setup(
         name="dsge",
         version="0.1.3",
         platforms="linux",
-        packages=find_packages(),
+        packages=["dsge"],
         test_suite="nose.collector",
         tests_require=["nose"],
         package_data={
-            "dsge": [
-                "examples/ar1/*",
-                "examples/DGS/*",
-                "examples/edo/*",
-                "examples/nkmp/*",
-                "examples/schorf_phillips_curve/*",
-                "examples/simple-model/*",
-                "examples/sw/*",
-            ]
-        },
-        install_requires=["pandas", "scipy", "sympy", "pyyaml", "numba"],
+             "dsge": [
+                 "examples/ar1/*",
+                 "examples/DGS/*",
+                 "examples/edo/*",
+                 "examples/nkmp/*",
+                 "examples/schorf_phillips_curve/*",
+                 "examples/simple-model/*",
+                 "examples/sw/*",
+             ]
+         },
+        requires=["pandas", "scipy", "sympy", "pyyaml", "numba"],
     )
