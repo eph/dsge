aaa8aaccc30f496c0c255bcd6f2d7d47bd480441
Ed Herbst <ed.herbst@gmail.com>
2025-09-11 22:51:51 -0400

    adjust tests

--- dsge/tests/test_cpp_export.py
+++ dsge/tests/test_cpp_export.py
@@ -1,13 +1,13 @@
 import unittest
 from dsge import read_yaml
+from dsge.resource_utils import resource_path
 from dsge.translate import translate
 
 class TestCppExport(unittest.TestCase):
     def test_cpp_export_for_fhp_model(self):
-        # Assuming an FHP model YAML file exists in 'examples/fhp.yaml'
-        #example_yaml_path = os.path.join(os.path.dirname(__file__), "..", "examples", "fhp", "fhp.yaml")
-        example_yaml_path = '/home/eherbst/Dropbox/tmp/fhp.yaml'
-        model = read_yaml(example_yaml_path)
+        # Use the in-repo FHP example
+        with resource_path('examples/fhp/fhp.yaml') as p:
+            model = read_yaml(str(p))
 
         # Calling translate with language "cpp" should trigger NotImplementedError.
         #with self.assertRaises(NotImplementedError) as context:
